931209614258156f38ccbe64976b351caf33aa13
anilboppuri <anil4java2@gmail.com>
2022-03-02 21:59:31 +0530

    added resource class

---
+++ src/main/java/com/cts/pipeline/resource/Connect.java
@@ -0,0 +1,17 @@
+package com.cts.pipeline.resource;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/check")
+public class Connect {
+	
+	@GetMapping
+	public String ready() {
+		
+		return "1.2.3.... ready";
+	}
+
+}
